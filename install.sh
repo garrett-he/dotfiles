645c00ef0dd2c379ce5cef575d106f31b47b4d9d
Garrett HE <garrett.he@hotmail.com>
2022-11-13 13:58:42 +0800

    Add support for configuration file

--- install.sh
+++ install.sh
@@ -22,10 +22,20 @@ dotfiles_install() {
     DOTFILES_ROOT=$(dirname "$(realpath "$0")")
     source "$DOTFILES_ROOT/functions.sh"
 
+    if [[ -f "$DOTFILES_ROOT"/config ]]; then
+        source "$DOTFILES_ROOT"/config
+    fi
+
     for DOTFILES_MODULE_DIR in "$DOTFILES_ROOT"/modules/*; do
         DOTFILES_MODULE_NAME=$(basename "$DOTFILES_MODULE_DIR")
 
-        if dotfiles_confirm "Install module: $DOTFILES_MODULE_NAME"; then
+        local module_install_toggle=install_${DOTFILES_MODULE_NAME}
+
+        if [[ -n "${!module_install_toggle}" ]]; then
+            module_install_toggle="${!module_install_toggle}"
+        fi
+
+        if dotfiles_confirm "Install module: $DOTFILES_MODULE_NAME" || dotfiles_is_true "$module_install_toggle"; then
             echo
             (
                 source "$MODULE_DIR"/SETUP
