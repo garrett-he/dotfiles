752122ce1190d9f4a636bf1228213d5314a36ba7
Garrett HE <garrett.he@hotmail.com>
2022-11-15 13:12:26 +0800

    Add function to get OSTYPE

--- lib/env.sh
+++ lib/env.sh
@@ -1,3 +1,13 @@
 env::shell_name() {
     basename $(readlink /proc/$$/exe)
 }
+
+env::ostype() {
+    if [[ $OSTYPE == "darwin"* ]]; then
+        echo darwin
+    elif [[ $OSTYPE == "linux"* ]]; then
+        echo linux
+    else
+        echo $OSTYPE
+    fi
+}
--- spec/env_spec.sh
+++ spec/env_spec.sh
@@ -6,4 +6,27 @@ Describe 'module env'
             The stdout should eq $(basename $(readlink /proc/$$/exe))
         End
     End
+
+    Describe 'function ostype'
+        It 'should return "linux" on Linux systems.'
+            OSTYPE='linux-gnu'
+            When call env::ostype
+            The stdout should eq 'linux'
+        End
+        It 'should return "darwin" on macOS systems.'
+            OSTYPE='darwin22.0'
+            When call env::ostype
+            The stdout should eq 'darwin'
+        End
+        It 'should return "cygwin" on Cygwin systems.'
+            OSTYPE='cygwin'
+            When call env::ostype
+            The stdout should eq 'cygwin'
+        End
+        It 'should return "msys" on MSYS2 systems.'
+            OSTYPE='msys'
+            When call env::ostype
+            The stdout should eq 'msys'
+        End
+    End
 End
