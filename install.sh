b7eead94679bfae7392a0a406a065f057ce7057f
Garrett HE <garrett.he@outlook.com>
2024-08-11 10:49:30 +0800

    Add support for configurations

--- install.sh
+++ install.sh
@@ -25,8 +25,18 @@ dotfiles_announce() {
 }
 
 dotfiles_install() {
+    if [[ -f "$DOTFILES_ROOT"/config ]]; then
+        source "$DOTFILES_ROOT"/config
+    fi
+
     for DOTFILES_MODULE_DIR in "$DOTFILES_ROOT"/modules/*; do
         DOTFILES_MODULE_NAME=$(basename "$DOTFILES_MODULE_DIR")
+
+        local module_install_toggle=install_${DOTFILES_MODULE_NAME}
+
+        if [[ -n "${!module_install_toggle-}" ]]; then
+            module_install_toggle="${!module_install_toggle}"
+        fi
 
         if utils::confirm "Install module: $DOTFILES_MODULE_NAME" || utils::is_true "$module_install_toggle"; then
             echo
